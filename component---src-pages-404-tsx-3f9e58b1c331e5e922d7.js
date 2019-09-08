6eae2d770daa2c1952cea8a86622ae3cb2250d8e
Shawn Rivers <kaushou@gmail.com>
2019-09-08 16:02:30 +0900

    Updates

---
+++ component---src-pages-404-tsx-3f9e58b1c331e5e922d7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{264:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t(104),o=t(133),l=(t(265),t(9)),u=t(16);n.default=function(){return a.createElement(o.a,null,a.createElement("h1",null,"Page Not Found"),a.createElement(c.a,{to:Object(u.b)(l.e.Singles)},"Go back"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3f9e58b1c331e5e922d7.js.map
